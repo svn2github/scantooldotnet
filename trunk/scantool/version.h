60922aff6a46cf708b44daf340a0dddcb694141b
vit <vit@758b567e-1a1c-0410-849d-9887a4e3ecfe>
2005-12-17 09:11:50 +0000

    Updated version number
    git-svn-id: svn://svn.code.sf.net/p/scantool/code@26 758b567e-1a1c-0410-849d-9887a4e3ecfe

--- trunk/scantool/version.h
+++ trunk/scantool/version.h
@@ -2,12 +2,12 @@
 #define VERSION_H
 
 #define SCANTOOL_VERSION          1
-#define SCANTOOL_SUB_VERSION      09
+#define SCANTOOL_SUB_VERSION      10
 #define SCANTOOL_WIP_VERSION      0
-#define SCANTOOL_VERSION_STR      "1.09"
-#define SCANTOOL_VERSION_EX_STR   "1.09"
+#define SCANTOOL_VERSION_STR      "1.10"
+#define SCANTOOL_VERSION_EX_STR   "1.10"
 #define SCANTOOL_YEAR_STR         "2005"
-#define SCANTOOL_DATE             20050601    /* yyyymmdd */
+#define SCANTOOL_DATE             20051217    /* yyyymmdd */
 
 #ifdef ALLEGRO_WINDOWS
    #define SCANTOOL_PLATFORM_STR   "Windows"
